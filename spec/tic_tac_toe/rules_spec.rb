cc3ce14962b6248385ed204f3431a3dd306d772e
Eric Meyer <emeyer@8thlight.com>
2015-04-05 13:39:08 -0500

    Rules finds a winner in the first row

--- spec/tic_tac_toe/rules_spec.rb
+++ spec/tic_tac_toe/rules_spec.rb
@@ -3,9 +3,30 @@ require "tic_tac_toe/rules"
 describe TicTacToe::Rules do
 
   describe "Determing if the game has a winner" do
+    before(:each) do
+      @rules = TicTacToe::Rules.new
+    end
+
     it "has no winner for an empty board" do
-      rules = TicTacToe::Rules.new
-      expect(rules).to_not have_winner([nil, nil, nil,
+      expect(@rules).to_not have_winner([nil, nil, nil,
+                                        nil, nil, nil,
+                                        nil, nil, nil])
+    end
+
+    it "has a winner in the first row" do
+      expect(@rules).to have_winner(["X", "X", "X",
+                                    nil, nil, nil,
+                                    nil, nil, nil])
+    end
+
+    it "does not have a winner in the row when the marks are different" do
+      expect(@rules).to_not have_winner(["X", "O", "X",
+                                        nil, nil, nil,
+                                        nil, nil, nil])
+    end
+
+    it "does not have a winner in the row when it is only partially filled" do
+      expect(@rules).to_not have_winner(["X", nil, nil,
                                         nil, nil, nil,
                                         nil, nil, nil])
     end
