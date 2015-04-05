63224ee40e0d0058adac60eef800882e6b34d598
Eric Meyer <emeyer@8thlight.com>
2015-04-05 13:46:26 -0500

    Extract filled_with_the_same?

--- lib/tic_tac_toe/rules.rb
+++ lib/tic_tac_toe/rules.rb
@@ -5,8 +5,14 @@ module TicTacToe
       row_one = [0, 1, 2].map {|position| board[position] }
       row_two = [3, 4, 5].map {|position| board[position] }
       [row_one, row_two].any? do |row|
-        row.none?(&:nil?) && row.uniq.size == 1
+        filled_with_the_same?(row)
       end
+    end
+
+    private
+
+    def filled_with_the_same?(row)
+      row.none?(&:nil?) && row.uniq.size == 1
     end
 
   end
