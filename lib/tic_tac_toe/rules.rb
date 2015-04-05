80a5575bc4f12931cefb3ff8103d6a96107c2c3e
Eric Meyer <emeyer@8thlight.com>
2015-04-05 13:53:40 -0500

    Create a board inside of the method

--- lib/tic_tac_toe/rules.rb
+++ lib/tic_tac_toe/rules.rb
@@ -1,9 +1,28 @@
 module TicTacToe
+  class Board
+    def initialize(board_as_array)
+      self.board_as_array = board_as_array
+    end
+
+    def row(number)
+      if number == 1
+        [0, 1, 2].map {|position| board_as_array[position] }
+      else
+        [3, 4, 5].map {|position| board_as_array[position] }
+      end
+    end
+
+    private
+
+    attr_accessor :board_as_array
+  end
+
   class Rules
 
-    def has_winner?(board)
-      row_one = [0, 1, 2].map {|position| board[position] }
-      row_two = [3, 4, 5].map {|position| board[position] }
+    def has_winner?(board_as_array)
+      board = Board.new(board_as_array)
+      row_one = board.row(0)
+      row_two = board.row(1)
       [row_one, row_two].any? do |row|
         filled_with_the_same?(row)
       end
